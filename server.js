566fd0c5c572b0651c63d5682c86795ff70e92b4
danpetrow <dppetrow@gmail.com>
2022-04-02 11:31:18 -0400

    static

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const app = express();
- 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello server is running')
-    .end();
-});
- 
+
+app.use(express.static('./public'))
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
