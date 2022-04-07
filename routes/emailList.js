c6da8a6d50a72ac2f41ddcaa1b1cdb0980b3c8b8
danpetrow <dppetrow@gmail.com>
2022-04-06 22:44:35 -0400

    email validation

--- routes/emailList.js
+++ routes/emailList.js
@@ -2,62 +2,43 @@ const express = require('express')
 const router = express.Router()
 const connection = require('../model/db')
 const path = require('path')
-const { body } = require('express-validator');
+const { body, check, validationResult } = require('express-validator');
 
 // TODO handle if body email is not email & add other other validators for body('firstName') and ('lastName'),
-router.post('/', body('email').isEmail(), (req, res, next) => {
+router.post('/', check('email', 'Your email is not valid').not().isEmpty().isEmail().normalizeEmail(),
+(req, res, next) => {
     // Capture the input fields
     let email = req.body.email
     let firstName = req.body.firstName
     let lastName = req.body.lastName
 
-    connection.query(`SELECT * FROM emaillist WHERE LOWER(email) = LOWER('${req.body.email}')`,[email],
-        (err, result) => {
-            if (result.length) {
-                return res.status(409).send({
-                    msg: "You're already signed up!"
-                });
-            } else {
-                connection.query(`insert into emaillist (firstName,lastName,email,subscribed) values ('${firstName}','${lastName}','${email}',0)`, [email, firstName, lastName],
-                    (err, result) => {
-                        if (err) {
-                            throw err;
-                            return res.status(400).send({
-                                msg: err
-                            });
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).jsonp(errors.array())
+    }
+    else {
+        connection.query(`SELECT * FROM emaillist WHERE LOWER(email) = LOWER('${email}')`, [email],
+            (err, result) => {
+                if (result.length) {
+                    return res.status(409).send({
+                        msg: "You're already signed up!"
+                    });
+                } else {
+                    connection.query(`insert into emaillist (firstName,lastName,email,subscribed) values ('${firstName}','${lastName}','${email}',0)`, [email, firstName, lastName],
+                        (err, result) => {
+                            if (err) {
+                                throw err;
+                                return res.status(400).send({
+                                    msg: err
+                                });
+                            }
+                            return req.session.signedUp = true, req.session.firstName = firstName, res.redirect('/email')
                         }
-                        return req.session.signedUp= true, req.session.firstName = firstName, res.redirect('/email')
-                    }
-                );
-            }
-        });
+                    );
+                }
+            });
+    }
 })
-
-
-// 		// Execute SQL query that'll select the account from the database based on the specified username and password
-// 		connection.query(`insert into emaillist (firstName,lastName,email,subscribed) values ('${firstName}','${lastName}','${email}',0)`, [email, firstName, lastName], function(error, results, fields) {
-// 			// If there is an issue with the query, output the error
-// 			if (error) throw error;
-//             else {
-//                 req.session.loggedin = true;
-// 				req.session.firstName = firstName;
-//                 res.redirect('/email');
-// 			}			
-// 			res.end();
-// 		});
-//         }
-//         catch(err){
-//             if (err.code === 'ER_DUP_ENTRY') {
-//                 //handleHttpErrors(SYSTEM_ERRORS.USER_ALREADY_EXISTS);
-//             } else {
-//                 //handleHttpErrors(err.message);
-//              }
-//         }
-// 	} else {
-// 		res.send('Please enter your name and email to join the list!');
-// 		res.end();
-// 	}
-// });
 
 router.get('/', (req, res) => {
     // If the user is loggedin
